e9ad9ddb718019c436264dc8f16970ce7b153108
Paul Hood <hoodp@mail.gvsu.edu>
2015-07-26 13:55:25 -0400

    updated trust all script

--- trustAll.bash
+++ trustAll.bash
@@ -8,14 +8,17 @@ _trust_host() {
 }
 
 # trust all eos machines
-for i in {1..32}; do _trust_host "eos$(printf '%02d' $i)"; done
+for i in {1..32}; do 
+    _trust_host "eos$(printf '%02d' $i)"; 
+done
 
 # trust all arch machines
-for i in {1..10}; do _trust_host "arch$(printf '%02d' $i)"; done
+for i in {1..10}; do 
+    _trust_host "arch$(printf '%02d' $i)"; 
+done
 
 # trust all datacomm machines
-for i in {1..24}; do _trust_host "dc$(printf '%02d' $i)"; done
-
-# automatic login
-ssh eos08
+for i in {1..24}; do 
+    _trust_host "dc$(printf '%02d' $i)"; 
+done
 
